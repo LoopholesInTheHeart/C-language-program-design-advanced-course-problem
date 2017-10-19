d1d0832ab45cb30e6dd083c847e03e00bddbeff9
HaretDust <tt1296116093@outlook.com>
2017-10-19 11:12:21 +0800

    Update C10-3.cpp

--- C10-3.cpp
+++ C10-3.cpp
@@ -1,3 +1,23 @@
+/*
+字符串出现个数  (100/100 分数)
+题目描述
+给定一个串a和串b，求b在a中出现次数
+
+（友情提示可以使用stl::count函数）
+
+输入描述
+字符串a,b的长度1<= len(a)<=100, 1<=len(b)<=len(a)
+
+输出描述
+一个数字
+
+样例输入
+ababac
+aba
+
+样例输出
+2
+*/
 #include <iostream>
 #include <cstdlib>
 #include <string>
